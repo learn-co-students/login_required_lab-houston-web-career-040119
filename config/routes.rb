d85ed43897820d524ce9b97f8a2cd6844fed5b0b
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-03 23:50:53 -0500

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :secrets, only: [:show]
   resources :sessions, only: [:new,:create,:destroy,:show]
   get '/secrets/show' => 'secrets#show', as: :show
   delete '/' => 'sessions#destroy', as: :destroy
